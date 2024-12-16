6bdcdd573d45fe9596f45cadf6307b05ffbf02d4
Andre Pangestu <andrepangestu@broom.id>
2024-12-16 20:49:38 +0700

    fix remove dark mode default

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       */}
       <head />
 
-      <body className="">
+      <body>
         <Providers>
           <Header />
           <div className="min-h-screen w-full">{children}</div>
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
   return (
     <>
       <ScrollUp />
-
       <BannerCarousel />
       <Features />
       <ProductOverview />
