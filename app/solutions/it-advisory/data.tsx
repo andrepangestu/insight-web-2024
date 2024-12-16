995ea7a8a7bf893a44ec6e0b49c5cacb4dcadde1
Andre Pangestu <andrepangestu@Andres-MacBook-Pro.local>
2024-12-16 21:18:34 +0700

    finish content menu solutions

---
+++ app/solutions/it-advisory/data.tsx
@@ -0,0 +1,28 @@
+export const independentReviewData = [
+    {
+      id: 1,
+      imageSrc: "/images/solutions/it-advisory/it-advisory-1.svg",
+      imageAlt: "it-advisory-1",
+      title: "Independent IT Governance",
+      content:
+        "Our IT governance service offers executive teams an unbiased resource to define and assess both current and future business states, ensuring effective execution and alignment with strategic goals..",
+    },
+    {
+      id: 2,
+      imageSrc: "/images/solutions/it-advisory/it-advisory-2.svg",
+      imageAlt: "it-advisory-2",
+      title: "Actionable Insights",
+      content:
+        "At Insight Consulting, we help organizations navigate the IT landscape by evaluating their current position and identifying goals. We provide practical recommendations that lead to significant milestones in your IT journey.",
+      reverse: true,
+    },
+    {
+        id: 3,
+        imageSrc: "/images/solutions/it-advisory/it-advisory-3.svg",
+        imageAlt: "it-advisory-3",
+        title: "Comprehensive Support",
+        content:
+          "We deliver frameworks, processes, and toolkits tailored to your needs, whether optimizing existing systems or initiating governance for the first time. With our guidance, together we can turn your IT goals into reality.",
+      },
+  ];
+  
