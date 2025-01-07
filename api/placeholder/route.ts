7b13fcaf9353fc8adf4aa9f9841500460b88b5e2
Andre Pangestu <andrepangestu@broom.id>
2025-01-07 17:23:39 +0700

    fix update banner home

--- api/placeholder/route.ts
+++ api/placeholder/route.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await response.json();
-    return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch data" },
-      { status: 500 }
-    );
-  }
-}
+// export async function GET() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     const data = await response.json();
+//     return NextResponse.json(data, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "Failed to fetch data" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export async function POST() {
-  return NextResponse.json(
-    { error: "Method POST Not Allowed" },
-    { status: 405 }
-  );
-}
+// export async function POST() {
+//   return NextResponse.json(
+//     { error: "Method POST Not Allowed" },
+//     { status: 405 }
+//   );
+// }
